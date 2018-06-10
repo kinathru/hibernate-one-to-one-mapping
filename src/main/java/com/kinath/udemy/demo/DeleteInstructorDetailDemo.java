d108cd06dcad779fdb851f020e450df8292c5212
kinath <kinath.ru@gmail.com>
2018-06-10 16:35:57 +0530

    KR:Hibernate:one-to-one mapping cascade bi directional delete

---
+++ src/main/java/com/kinath/udemy/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,35 @@
+package com.kinath.udemy.demo;
+
+import com.kinath.udemy.entity.Instructor;
+import com.kinath.udemy.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteInstructorDetailDemo
+{
+    public static void main( String[] args )
+    {
+        SessionFactory factory = new Configuration().configure( "hibernate.cfg.xml" ).addAnnotatedClass( Instructor.class ).addAnnotatedClass( InstructorDetail.class ).buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        try
+        {
+            session.beginTransaction();
+
+            int theId = 2;
+            InstructorDetail instructorDetail = session.get( InstructorDetail.class, theId );
+            System.out.println( instructorDetail.toString() );
+            System.out.println( instructorDetail.getInstructor().toString() );
+
+            System.out.println("Deleting Instructor Detail : ");
+            session.delete( instructorDetail );
+
+            session.getTransaction().commit();
+        }
+        finally
+        {
+            factory.close();
+        }
+    }
+}
